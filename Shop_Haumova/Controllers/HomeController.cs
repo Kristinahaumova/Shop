4dc10cc201b4be8fee2f582da8c7691fa034abaa
Kristinahaumova <kristinahaumova@gmail.com>
2025-09-23 22:33:58 +0500

    Разработанное перенаправление на страницу с предметами. Хаюмова

---
+++ Shop_Haumova/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop_Haumova.Controllers
+{
+    public class HomeController : Controller
+    {
+        public RedirectResult Index() 
+        {
+            return Redirect("/Items/List");
+        }
+    }
+}
